eee8ef3f31dad5cdd7f1ce3758a3a140f232794f
Prakhar Shukla <prakshukla@deloitte.com>
2018-05-01 02:23:07 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if(err){
+            console.log('unable to append to server.log');
+        }
+    });
+    next();
+});
+
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs',{
+//         pageTitle: 'Maintenance Page',
+//         message: 'Our website is under maintenance. Please come agin later !'
+//     });
+// });
+
+app.use(express.static(__dirname+"/public"));
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+})
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        message: 'Welcome Buddy !'
+    });
+});
+
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page'
+    });
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+        name: "Bad Request",
+        errorMessage: "Something went wrong!"
+    })
+})
+
+app.listen(3000, () => {
+    console.log("Listening to port 3000");
+});
